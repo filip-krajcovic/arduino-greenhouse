21d346c88381e0799fedef56f1f803dea11bf2ee
Tibor Krajčovič <dev.tiqra@gmail.com>
2023-04-13 20:33:07 +0200

    prvy nastrel

---
+++ arduino_secrets.h
@@ -0,0 +1,7 @@
+#define SECRET_SSID "TENET"
+#define SECRET_PASS "8LP3B&5tW"
+#define SECRET_MQTT_CLIENT_ID "BD80439B277B163A"
+#define SECRET_MQTT_HOST "mqtt.akenza.io"
+#define SECRET_MQTT_USERNAME "073b2a29dbd1f070"
+#define SECRET_MQTT_PASSWORD "wi0di4tgpdjuw09imxxwdx5wggwgd185"
+#define SECRET_MQTT_OUT_TOPIC "/up/wi0di4tgpdjuw09imxxwdx5wggwgd185/id/99F736739CA11D3B"
